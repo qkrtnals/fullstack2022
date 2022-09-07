1451703704b528a39334be447efc6d172fbe4c70
qkrtnals <qkrtnals0102@g.shingu.ac.kr>
2022-09-07 16:17:34 +0900

    11

---
+++ 02analogclock-t/aclock.js
@@ -0,0 +1,24 @@
+var houtHand = document.querySelector('[data-hour-hand]')
+var minuteHand = document.querySelector('[data-minute-hand]')
+var secondHand = document.querySelector('[data-secondHand-hand]')
+
+function setClock(){
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = currentDate.getMinutes() / 60
+    const hoursRatio = currentDate.getHours() / 12
+
+
+    setRotation(houtHand, hoursRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(secondHand, secondsRatio)
+}
+
+
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+setInterval(setClock, 1000)
